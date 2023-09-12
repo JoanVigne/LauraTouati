5a30ee3a662d59a7e800892f12fd24300cd0e196
Joan <joan.vigne@hotmail.fr>
2023-09-12 13:33:04 +0100

    first commit

---
+++ js/carousell.js
@@ -0,0 +1,6 @@
+let containerCarousell = document.querySelector(".carousell-container");
+
+let arrows = document.querySelectorAll(".arrow");
+
+/* containerCarousell.style.backgroundImage = "url('./assets/img/accueil-2.jpg')";
+ */
---
+++ js/goTop.js
@@ -0,0 +1,17 @@
+function basicScrollTop() {
+  let buttonToTop = document.createElement("a");
+  buttonToTop.setAttribute("id", "goTop");
+  document.body.append(buttonToTop);
+  buttonToTop.setAttribute("href", "#");
+
+  function btnReveal() {
+    if (window.scrollY >= 300) {
+      buttonToTop.classList.add("is-visible");
+    } else {
+      buttonToTop.classList.remove("is-visible");
+    }
+  }
+  window.addEventListener("scroll", btnReveal);
+}
+
+basicScrollTop();
